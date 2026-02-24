c550e9a244eb2cd494d5493ea91ce11bf3803c95
Khushali1505 <khushalivadaliya15@gmail.com>
2026-02-24 21:55:05 +0530

    Create examples.c

---
+++ day05_notes/examples.c
@@ -0,0 +1,23 @@
+// Array example
+int arr[3] = {10, 20, 30};
+
+// Pointer to array
+int *p = arr;
+
+// Access using index
+int value1 = arr[1];      // 20
+
+// Access using pointer
+int value2 = *(p + 1);    // 20
+
+// Global variable example
+int global_var = 5;
+
+int main(void)
+{
+    int local_var = 2;
+
+    while(1)
+    {
+    }
+}
